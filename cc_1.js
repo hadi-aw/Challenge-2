2afb12bfbe371123be96ec1d3c275a662991671a
Hadi Awad <hawad.abdrabbou@gmail.com>
2024-05-17 18:13:09 -0400

    cc_1.js
    "Create and test calcTip function."

--- cc_1.js
+++ cc_1.js
@@ -6,3 +6,12 @@ const total = bill + tip;
 // Task 2: Outputs Details
 console.log(`The bill was $${bill}, the tip was $${tip}, and the total value $${total}.`)
 
+// Task 3: Create a  Function
+function calcTip(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+ }
+
+ // Test the function with a bill value of $100
+ const testbill = 100;
+ console.log(`The tip for a bill of $${testbill} is $${calcTip(testbill)}.`);
+ 
